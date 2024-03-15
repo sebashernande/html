180933aef07af599d05f24dc55be68f5c807fd73
sebashernande <sebasrex150508@gmail.com>
2024-03-15 10:55:09 -0600

    Switch3

--- src/switch.js
+++ src/switch.js
@@ -1,38 +1,83 @@
 document.querySelector('#header').innerHTML = "estoy leyendo el documento switch"
-var fruta
+var Dia
 var mensaje
-var userInput = prompt("Ingrese el numero de la fruta")
+var userInput = prompt("Ingrese el numero del dia de la semana")
 
-function jugo_de_fresas() {
+function lunes() {
+    mensaje = "Lunes"
     document.querySelector('#header').innerHTML = mensaje;
     document.querySelector('#header').style.color = "red";
   
   }
   
-  function jugo_de_naranja() {
-    mensaje = "batir naranjas y mandarinas"
+  function martes() {
+    mensaje = "Martes"
     document.querySelector('#header').innerHTML = mensaje;
-    document.querySelector('#header').style.color = "red";
-  
-  }
+    document.querySelector('#header').style.color = "blue";
+}
 
-  function servir_vaso_de_agua() {
-    mensaje = "sirvame vaso con awa";
+  function Miercoles() {
+    mensaje = "Miercoles"
     document.querySelector('#header').innerHTML = mensaje;
     document.querySelector('#header').style.color = "pink";
+  
   }
 
-  fruta = parseInt(userInput)// con parseInt se convierte a entero 
-switch(fruta) {
+  function ese_no_es_un_dia() {
+    mensaje = "Ese no es un dia";
+    document.querySelector('#header').innerHTML = mensaje;
+    document.querySelector('#header').style.color = "red";
+  }
+
+  function Jueves() {
+    mensaje = "Jueves";
+    document.querySelector('#header').innerHTML = mensaje;
+    document.querySelector('#header').style.color = "purple";
+  }
+
+  
+  function Viernes() {
+    mensaje = "Viernes";
+    document.querySelector('#header').innerHTML = mensaje;
+    document.querySelector('#header').style.color = "brown";
+  }
+
+  function Sabado() {
+    mensaje = "Sabado";
+    document.querySelector('#header').innerHTML = mensaje;
+    document.querySelector('#header').style.color = "green";
+  }
+
+  function Domingo() {
+    mensaje = "Domingo";
+    document.querySelector('#header').innerHTML = mensaje;
+    document.querySelector('#header').style.color = "black";
+  }
+
+  Dia = parseInt(userInput)// con parseInt se convierte a entero 
+switch(Dia) {
     case 1:
-        mensaje = "batir fresas"
-        jugo_de_fresas()
-    break
+        lunes()
+        break
     case 2:
+        martes()
+        break
     case 3:
-        jugo_de_naranja()
+        Miercoles()
+        break
+    case 4:
+        Jueves()
+        break
+    case 5:
+        Viernes()
+        break
+    case 6:
+        Sabado()  
+        break 
+    case 7:
+        Domingo()
     break
     default:
-        servir_vaso_de_agua()
+        ese_no_es_un_dia()
     break
 }
