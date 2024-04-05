9a1c7160514f8d4f200b559cf392850ba56accf2
sebashernande <sebasrex150508@gmail.com>
2024-04-05 07:21:44 -0600

    WhileSwitch

---
+++ src/while.js
@@ -0,0 +1,12 @@
+document.querySelector('#header').innerHTML = "estoy leyendo el documento while.js"
+var mensaje
+
+frutas = ["Fresa","Pera","Naranja","Mango","Pera","Cereza"]
+
+var i = 5
+while(i >= 0){
+
+   console.log(i+ ". " + frutas[i])
+   i = i - 1
+}
+
---
+++ src/whileSwitch.js
@@ -0,0 +1,59 @@
+var mensaje;
+mensaje = "Estoy leyendo el documento whileSwitch.js"
+var elemento = document.querySelector('#header');
+elemento.innerHTML = mensaje 
+
+function suma() {
+    var Numero1 = prompt("Ingrese numero 1")  
+    var Numero2 = prompt("Ingrese numero 2")
+     alert(parseInt(Numero1)+parseInt(Numero2))
+  }
+  
+  function Resta() {
+    var Numero1 = prompt("Ingrese numero 1")
+    var Numero2 = prompt("Ingrese numero 2")
+    alert(parseInt(Numero1)-parseInt(Numero2))
+}
+
+  function Multiplicacion() {
+    var Numero1 = prompt("Ingrese numero 1")
+    var Numero2 = prompt("Ingrese numero 2")
+    alert(parseInt(Numero1)*parseInt(Numero2))
+  
+  }
+
+  function Division() {
+    var Numero1 = prompt("Ingrese numero 1")
+    var Numero2 = prompt("Ingrese numero 2")
+    alert(parseInt(Numero1)/parseInt(Numero2))
+  }
+
+
+var inicio = true
+
+while(inicio == true){
+    var respuesta = prompt("1. suma\n2. Resta\n3. Multiplicacion\n4. division\n5. cerrar programa")
+    switch(parseInt(respuesta)){
+         case 1:
+          suma()
+         break
+
+         case 2:
+          Resta()
+         break
+
+          case 3:
+          Multiplicacion()
+         break
+
+         case 4:
+          Division()
+         break
+
+         case 5:
+            inicio = false 
+    
+    
+    }
+      }
+    
