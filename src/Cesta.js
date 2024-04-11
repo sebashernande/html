b672aff876368ebeb480f636034606ddc26e6e8a
sebashernande <sebasrex150508@gmail.com>
2024-04-11 10:03:31 -0600

    cesta3

--- src/Cesta.js
+++ src/Cesta.js
@@ -2,6 +2,7 @@
 
  var cesta = ["Fresa", "Pera", "Pina", "Naranja","Uva"]
   
+ var x = 0
 
  function Elementos_de_la_cesta() {
   alert(cesta)
@@ -17,6 +18,19 @@
     cesta.pop()
     alert("Se elimino con exito")
       }
+
+      
+    function Quitar_una_Fruta_espesicifca() {
+     var frutapaelimirar = prompt ("Que fruta desea eliminar?")
+
+      do{
+        x + 1 
+
+      
+
+        cesta.splice(x, frutapaelimirar)
+      }while(x <= cesta.length)
+        }
 
 var inicio = true
 do{
@@ -34,7 +48,11 @@ do{
        Quitar_una_Fruta()
        break
 
-      case 4:
+       case 4:
+       Quitar_una_Fruta_espesicifca()
+       break
+
+      case 5:
           inicio = prompt("Desea terminar ciclo?")
           if (inicio == "si"){
               inicio = false
